504e30ca21f15c4cb6ec82826a63368bcdd5bc10
xianpf <xianpf@xianpfs-MacBook-Pro.local>
2019-12-09 16:53:22 +0800

    finish grow model

--- fcos_core/modeling/backbone/growresnext.py
+++ fcos_core/modeling/backbone/growresnext.py
@@ -169,6 +169,12 @@ class GrowResNeXtBottleneck(nn.Module):
 
         self.downsample = None
         self.embryo_channels = 8
+        self.embryo_grow_tic = 0
+        self.embryo_grow_tic_threshold = 2
+        self.adults_volume = 32
+        self.adults_channels = self.embryo_channels * self.adults_volume
+        self.adults_occupied = torch.zeros((self.adults_volume))
+        self.adults_bns = [None] * self.adults_volume
         if in_channels != out_channels:
             down_stride = stride if dilation == 1 else 1
             self.downsample = nn.Sequential(
@@ -185,7 +191,7 @@ class GrowResNeXtBottleneck(nn.Module):
 
         if dilation > 1:
             stride = 1 # reset to be 1
-        # 策略： embryo和adults采用不同的optimizer，embryo长得快，成熟期或掐表或判定。
+        # 策略：Xembryo和adults采用不同的optimizer，embryo长得快，成熟期或掐表或判定。
         #       embryo成熟后，导入adults空白部分，相应的配套conv1 conv3 也要妥善转入。
         #       adults的导入要管理好。要测试bn导入前后的影响，不行就强行多adults_bn。
         #       adults的各个体要用进废退，市占率低的要有序退出，允许embryo再生。
@@ -194,43 +200,125 @@ class GrowResNeXtBottleneck(nn.Module):
 
         # stride_1x1, stride_3x3 = (stride, 1) if stride_in_1x1 else (1, stride)
 
-        self.embryo_conv1 = Conv2d(in_channels, self.embryo_channels, 1, 2, bias=False)
+        self.embryo_conv1 = Conv2d(in_channels, self.embryo_channels, 1, stride, bias=False)
+        self.embryo_bn1 = norm_func(self.embryo_channels)
+
+        self.embryo_conv2 = Conv2d(self.embryo_channels, self.embryo_channels, 3, 1, 
+                                    padding=dilation, bias=False, dilation=dilation)
+        self.embryo_bn2 = norm_func(self.embryo_channels)
+
+        self.embryo_conv3 = Conv2d(self.embryo_channels, out_channels, 1, bias=False)
+        self.embryo_bn3 = norm_func(out_channels)
+
         # TODO: specify init for self.conv1 1*1conv
-        self.embryo_bn1 = norm_func(bottleneck_channels)
-
-        self.embryo_conv2 = Conv2d(self.embryo_channels, self.embryo_channels, 3, 1, padding=dilation, bias=False, dilation=dilation)
         # TODO: try to init with spercific rules, or just init once then never change.
-        nn.init.kaiming_uniform_(self.conv2.weight, a=1)
-        self.bn2 = norm_func(self.embryo_channels)
+        nn.init.kaiming_uniform_(self.embryo_conv1.weight, a=1)
+        nn.init.kaiming_uniform_(self.embryo_conv2.weight, a=1)
+        nn.init.kaiming_uniform_(self.embryo_conv3.weight, a=1)
+        self.embryo_init_reserve = {
+            "embryo_conv1": self.embryo_conv1.state_dict(),
+            "embryo_conv2": self.embryo_conv2.state_dict(),
+            "embryo_conv3": self.embryo_conv3.state_dict(),
+            "embryo_bn1": self.embryo_bn1.state_dict(),
+            "embryo_bn2": self.embryo_bn2.state_dict(),
+            "embryo_bn3": self.embryo_bn3.state_dict(),
+        }
 
-        self.conv3 = Conv2d(self.embryo_channels, out_channels, kernel_size=1, bias=False)
-        self.bn3 = norm_func(out_channels)
+        self.adults_conv1 = Conv2d(in_channels, self.adults_channels, 1, stride, bias=False)
+        self.adults_conv2 = Conv2d(self.adults_channels, self.adults_channels, 3, 1, 
+                    padding=dilation, bias=False, dilation=dilation)
+                    # padding=dilation, bias=False, dilation=dilation, groups=self.adults_volume)
+        self.adults_conv3 = Conv2d(self.adults_channels, out_channels, 1, bias=False)
 
-        for l in [self.conv1, self.conv3,]:
-            nn.init.kaiming_uniform_(l.weight, a=1)
+        # TODO: adults 三层卷积的group和初始化要细心设计
+        self.adults_conv1.weight.data.zero_()
+        self.adults_conv2.weight.data.zero_()
+        self.adults_conv3.weight.data.zero_()
 
 
-        # TODO: adults_channels need a large channel volume. say max embryo_channels*32 = 8*32=256
+    def adult_ceremony(self):
+        # - 把embryo导入adults， 尽可能保持adults neat
+        available_space = torch.where(self.adults_occupied == 0)[0]
+        if len(available_space):
+            cell_idx = int(available_space[0])
+            cell_from = self.embryo_channels * cell_idx
+            cell_end = self.embryo_channels * (cell_idx + 1)
+        else:
+            import pdb; pdb.set_trace()
+        self.adults_conv1.weight.data[cell_from:cell_end] = self.embryo_conv1.weight.data
+        self.adults_conv2.weight.data[cell_from:cell_end, cell_from:cell_end] = self.embryo_conv2.weight.data
+        self.adults_conv3.weight.data[:,cell_from:cell_end] = self.embryo_conv3.weight.data
+        # print(self.adults_conv2.weight.data[0,:,1,1])
+
+        # - 对应的bn导入
+        if self.adults_bns[cell_idx]:
+            print("Error! self.adults_bns[{}] is not None.".format([cell_idx]))
+            import pdb; pdb.set_trace()
+        self.adults_bns[cell_idx] = [self.embryo_bn1, self.embryo_bn2, self.embryo_bn3]
+        print('Ids:', id(self.adults_bns[cell_idx][0]), id(self.adults_bns[cell_idx][1]), id(self.adults_bns[cell_idx][2]))
+
+        # - embryo 恢复到初始状态
+        self.embryo_conv1.load_state_dict(self.embryo_init_reserve['embryo_conv1'])
+        self.embryo_conv2.load_state_dict(self.embryo_init_reserve['embryo_conv2'])
+        self.embryo_conv3.load_state_dict(self.embryo_init_reserve['embryo_conv3'])
+        self.embryo_bn1.load_state_dict(self.embryo_init_reserve['embryo_bn1'])
+        self.embryo_bn2.load_state_dict(self.embryo_init_reserve['embryo_bn2'])
+        self.embryo_bn3.load_state_dict(self.embryo_init_reserve['embryo_bn3'])
+        import pdb; pdb.set_trace()
+        # - 探讨 8个embryo的关系，它们是一个整体，还是各自独立，或是其他
+
+
+    def adults_apply_bn(self, x, layer):
+        x_bn = torch.zeros_like(x)
+        for i, adult_bn in enumerate(self.adults_bns):
+            # import pdb; pdb.set_trace()
+            idx_from, idx_end = self.embryo_channels * i, self.embryo_channels * (i + 1)
+            if adult_bn:
+                import pdb; pdb.set_trace()
+            else:
+                x_bn[:,idx_from:idx_end,:,:] = x[:,idx_from:idx_end,:,:]
+        return x_bn
 
     def forward(self, x):
-        import pdb; pdb.set_trace()
+        self.embryo_grow_tic += 1
+        if self.embryo_grow_tic > self.embryo_grow_tic_threshold:
+            self.adult_ceremony()
+            self.embryo_grow_tic = 0
+            
+        # import pdb; pdb.set_trace()
         identity = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu_(out)
+        # embryo part
+        x_embryo_conv1 = self.embryo_conv1(x)
+        x_embryo_bn1 = self.embryo_bn1(x_embryo_conv1)
+        x_embryo_1 = F.relu_(x_embryo_bn1)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = F.relu_(out)
+        x_embryo_conv2 = self.embryo_conv2(x_embryo_1)
+        x_embryo_bn2 = self.embryo_bn2(x_embryo_conv2)
+        x_embryo_2 = F.relu_(x_embryo_bn2)
 
-        out0 = self.conv3(out)
-        out = self.bn3(out0)
+        x_embryo_conv3 = self.embryo_conv3(x_embryo_2)
+        x_embryo_bn3 = self.embryo_bn3(x_embryo_conv3)
+
+        # import pdb; pdb.set_trace()
+        x_adults_conv1 = self.adults_conv1(x)
+        # TODO: 整理好对应的bn并apply
+        x_adults_bn1 = self.adults_apply_bn(x=x_adults_conv1, layer=1)
+        x_adults_1 = F.relu_(x_adults_bn1)
+        x_adults_conv2 = self.adults_conv2(x_adults_bn1)
+        x_adults_bn2 = self.adults_apply_bn(x=x_adults_conv2, layer=2)
+        x_adults_2 = F.relu_(x_adults_bn2)
+        x_adults_conv3 = self.adults_conv3(x_adults_2)
+        x_adults_bn3 = self.adults_apply_bn(x=x_adults_conv3, layer=3)
+
+
+
 
         if self.downsample is not None:
             identity = self.downsample(x)
-
-        out += identity
+        if not x_embryo_bn3.shape==x_adults_bn3.shape==identity.shape:
+            import pdb; pdb.set_trace()
+        out = x_embryo_bn3 + x_adults_bn3 + identity
         out = F.relu_(out)
 
         return out
@@ -269,6 +357,7 @@ class GrowResNeXt(nn.Module):
             #         "with_modulated_dcn": cfg.MODEL.RESNETS.WITH_MODULATED_DCN, # false
             #         "deformable_groups": cfg.MODEL.RESNETS.DEFORMABLE_GROUPS,   # 1
             #     }
+            # Bottleneck(64, 64, 256, 1, True, 1, 1, FrozenBatchNorm2d, same_false_dcn_config),
             GrowResNeXtBottleneck(64, 64, 256, 1, True, 1, 1, FrozenBatchNorm2d, same_false_dcn_config),
             GrowResNeXtBottleneck(256, 64, 256, 1, True, 1, 1, FrozenBatchNorm2d, same_false_dcn_config),
             GrowResNeXtBottleneck(256, 64, 256, 1, True, 1, 1, FrozenBatchNorm2d, same_false_dcn_config),
@@ -296,7 +385,7 @@ class GrowResNeXt(nn.Module):
         # self.return_features = {'layer1': True, 'layer2': True, 'layer3': True, 'layer4': True}
 
         # Optionally freeze (requires_grad=False) parts of the backbone
-        self._freeze_backbone(cfg.MODEL.BACKBONE.FREEZE_CONV_BODY_AT)
+        # self._freeze_backbone(cfg.MODEL.BACKBONE.FREEZE_CONV_BODY_AT)
 
     def _freeze_backbone(self, freeze_at):
         if freeze_at < 0:
@@ -513,6 +602,8 @@ class Bottleneck(nn.Module):
 
         out0 = self.conv3(out)
         out = self.bn3(out0)
+
+        import pdb; pdb.set_trace()
 
         if self.downsample is not None:
             identity = self.downsample(x)
